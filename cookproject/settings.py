216f306b51eacabcc73edf1de88499a6a6f5e279
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-02-01 22:20:36 +0900

    iroiro

--- cookproject/settings.py
+++ cookproject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -137,5 +137,6 @@ LOGIN_REDIRECT_URL = 'accounts:home'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 
-    
 ]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
